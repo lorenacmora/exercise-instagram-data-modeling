6479ac92106eddd56a66854dd7f2d1b43ac3ac65
lorenacmora <lcma1981@gmail.com>
2023-03-08 19:40:34 +0000

    data

--- src/models.py
+++ src/models.py
@@ -8,31 +8,37 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True, unique=True, nullable=False)
+    usuario = Column(String(25), nullable=False)
+    nombre = Column(String(25), nullable=False)
+    apellido = Column(String(25), nullable=False)
+    correo = Column(String(25), unique=True)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Seguidor(Base):
+    __tablename__ = 'seguidor'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    user_from_id = Column(Integer, ForeignKey('user.id'))
+    user_to_id = Column(Integer, ForeignKey('user.id'))
+
+class Comentarios(Base):
+    __tablename__ = 'comentarios'
+    id = Column(Integer, primary_key=True)
+    comentario_text = Column(String(25))
+    autor_id = Column(Integer, ForeignKey('user.id'))
+    post_id = Column(Integer, ForeignKey('post.id'))
+
+class Post(Base):
+    __tablename__ = 'post'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+
 
     def to_dict(self):
         return {}
 
-## Draw from SQLAlchemy base
-try:
-    result = render_er(Base, 'diagram.png')
-    print("Success! Check the diagram.png file")
-except Exception as e:
-    print("There was a problem genering the diagram")
-    raise e
+
+
+
+render_er(Base, 'diagram.png')
